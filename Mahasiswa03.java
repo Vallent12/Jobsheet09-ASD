99af9e467f981bb209e3e7b12e917970d6d08754
Vallent12 <fevadtya09.gmail.com>
2026-04-24 22:03:15 +0700

    Pembetulan

--- Mahasiswa03.java
+++ Mahasiswa03.java
@@ -1,12 +1,12 @@
 public class Mahasiswa03 {
-    String nim;
     String nama;
+    String nim;
     String kelas;
     int nilai;
 
     Mahasiswa03(String nama, String nim, String kelas) {
-        this.nim = nama;
-        this.nama = nim;
+        this.nama = nama;
+        this.nim = nim;
         this.kelas = kelas;
         this.nilai = -1;
     }
